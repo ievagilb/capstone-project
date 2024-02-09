aa21db6274ff685e540fd6513561fc7a16a73c20
ievagilb <ievagilb@gmail.com>
2024-02-09 14:26:48 +0000

    Update finance_calculators.py
    comments added to the file

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,38 +1,7 @@
-'''
-load a greeting/welcome message
-write a variable to take in any variation of the words 'investment' or 'bond'
-
-calculate investment option:
-    
-    ask the user to input all the relevant information
-    ask the user to choose between 'simple' or 'compoud' options
-        
-        divide interest rate by 100
-        calculate 'simple' interest
-        print out the output
-
-        divide interest rate by 100
-        calculate 'compound' interest
-        round the answer to the last 2 decimel numbers
-        print out the output
-
-        if entered incorrect statement, let the user know
-
-calcute the bond option:
-    
-    ask the user to input all the relevant information
-    
-    calculate the monthly interest rate
-    calculate the repayment
-    round the answer to the last 2 decimel numbers
-    
-    print out the output
-
-ask the user to use a correct statement if entered incorrectly
-
-'''
-
 import math
+
+# load a greeting/welcome message
+# write a variable to take in any variation of the words 'investment' or 'bond'e
 
 greeting = input("investment - to calculate the amount of interest you'll earn on your investment\n\
 bond       - to calculate the amount you'll have to pay on a home loan\n\n\
@@ -40,6 +9,7 @@ Enter either 'investment' or 'bond' from the menu above to proceed: ")
 
 greeting = greeting.lower()
 
+# ask the user to input all the relevant information
 if greeting == "investment":
     
     money = int(input("Please enter the amount you would like to deposit:\n"))
@@ -47,25 +17,31 @@ if greeting == "investment":
     number_of_years = int(input("Please enter number of years you plan on investing:\n"))
     interest_type = input("Please choose if you would like simple or compound interest (enter 'simple' or 'compound'):\n")
     interest_type = interest_type.lower()    
+
+#ask the user to choose between 'simple' or 'compoud' options
+
+    # calculate simple interest
     
     if interest_type == "simple":
-
         interest_rate = interest_rate / 100
         simple_interest = money *(1 + interest_rate * number_of_years)
         print(f"You have earned a total of", simple_interest, "for this period")
-    
-    elif interest_type == "compound":
 
+    # calculate compund interest
+    elif interest_type == "compound":
         interest_rate = interest_rate / 100
         compound_interest = money * math.pow((1 + interest_rate),number_of_years)
         compound_interest = round(compound_interest, 2)
         print(f"You have earned a total of", compound_interest, "for this period") 
-    
     else:
         print("Please enter either 'simple' or compound'.\nPlease try again")
 
+# calcute the bond option
+
 elif greeting == "bond":
 
+    # ask the user to input all the relevant information
+    
     house_value = int(input("Please enter the value of the house:\n"))
     interest_rate = float(input("Please enter your interest rate as percentage (Example: 20):\n"))
     number_of_months = int(input("Please enter the number of months to repay the bond:\n"))
